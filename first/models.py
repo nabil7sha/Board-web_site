c4ac944f4afeb244ac75c03b7772d143448220d3
nabil7sha <nabil7sha@users.noreply.github.com>
2026-08-18 11:50:57 +0300

    main up

---
+++ first/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import Truncator
+
+class Board(models.Model):
+
+    name= models.CharField(max_length=50,unique=True)
+    description=models.CharField(max_length=150)
+
+    def __str__ (self):
+     return self.name
+
+    def get_post_count(self):
+        return Post.objects.filter(topic__Board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__Board=self).order_by('-created_dt').first()
+
+
+class Topic(models.Model):
+    subject=models.CharField(max_length=150)
+    Board=models.ForeignKey(Board,related_name='topics',on_delete=models.CASCADE)
+    created_by=models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
+    created_dt=models.DateTimeField(auto_now_add=True)
+    views=models.PositiveBigIntegerField(default=0)
+
+class Post(models.Model):
+        message=models.TextField(max_length=4000)
+        topic=models.ForeignKey(Topic,related_name='post',on_delete=models.CASCADE)
+        created_by=models.ForeignKey(User,related_name='post',on_delete=models.CASCADE)
+        created_dt=models.DateTimeField(auto_now_add=True)
+        updated_dt = models.DateTimeField(null=True)
+        def __str__(self):
+         truncted_message = Truncator(self.message)
+         return truncted_message.chars(30)
+# Create your models here.
---
+++ first/views.py
@@ -0,0 +1,126 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from django.http import HttpResponse,Http404
+from .models import Board
+from django.contrib.auth.models import User
+from .models import Topic,Post
+from django.utils.decorators import method_decorator
+from django.utils import timezone
+from django.views.generic import UpdateView
+
+from .forms import NewTopicForm,PostForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+# Create your views here.
+
+def home(request):
+
+    boards = Board.objects.all()
+    page = request.GET.get('page', 1)
+    
+    # عرض 6 بطاقات في كل صفحة (يمكنك تغيير الرقم)
+    paginator = Paginator(boards, 12) 
+    
+    try:
+        boards = paginator.page(page)
+    except PageNotAnInteger:
+        boards = paginator.page(1)
+    except EmptyPage:
+        boards = paginator.page(paginator.num_pages)
+    return render(request,'home.html',{'boards':boards})
+
+
+def board_topics(request,board_id):
+
+    board = get_object_or_404(Board,pk=board_id)
+    queryset = board.topics.order_by('-created_dt').annotate(comments=Count('post'))
+    page = request.GET.get('page',1)
+    paginator = Paginator(queryset,20)
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        topics = paginator.page(1)
+    except EmptyPage:
+        topics = paginator.page(paginator.num_pages)
+
+    return render(request,'topics.html',{'board':board,'topics':topics})
+
+
+
+@login_required
+def new_topic(request,board_id):
+    board = get_object_or_404(Board,pk=board_id)
+  #  user = User.objects.first()
+    if request.method == "POST":
+        form =NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.Board = board
+            topic.created_by = request.user
+            topic.save()
+
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                created_by = request.user,
+                topic=topic
+
+            )
+            return redirect('board_topics',board_id=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request,'new_topic.html',{'board':board,'form':form})
+
+
+
+def topic_posts(request,board_id,topic_id):
+     topic = get_object_or_404(Topic,Board__pk=board_id,pk=topic_id)  
+     session_key = 'view_topic_{}'.format(topic.pk)
+     if not request.session.get(session_key,False):
+        topic.views +=1
+        topic.save()
+        request.session[session_key] = True
+     return render(request,'topic_posts.html',{'topic':topic})
+
+
+
+@login_required
+def reply_topic(request, board_id,topic_id):
+    topic = get_object_or_404(Topic, Board__pk=board_id, pk=topic_id)
+    if request.method == "POST":
+        form =PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+
+            return redirect('topic_posts',board_id=board_id, topic_id = topic_id)
+    else:
+        form = PostForm()
+    return render(request,'reply_topic.html',{'topic':topic,'form':form})
+
+
+
+ 
+@method_decorator(login_required,name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_dt = timezone.now()
+        post.save()
+        return redirect('topic_posts', board_id=post.topic.Board.pk, topic_id=post.topic.pk)
+
+
+def about(request):
+
+    return HttpResponse(request,"yes")
